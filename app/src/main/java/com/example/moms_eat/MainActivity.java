dc4c9344bb79ecc5bf267e105db5e2ef75802174
Marline2 <Marline2@users.noreply.github.com>
2021-10-10 02:51:44 +0900

    *start page* add anim file for page animation fix toolbar.xml for menu icon
    *menu page*
    add SubActivity.java, subactivity_main.xml
    add menus and icons

--- app/src/main/java/com/example/moms_eat/MainActivity.java
+++ app/src/main/java/com/example/moms_eat/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.moms_eat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton menubtn = (ImageButton) findViewById(R.id.menu_btn);
+        menubtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.in_left, R.anim.layout_view);
+                finish();
+            }
+
+
+
+        });
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/example/moms_eat/SubActivity.java
@@ -0,0 +1,33 @@
+package com.example.moms_eat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subactivity_main);
+        Toast.makeText(this, "몸에 좋은 음식은 ♥를, \n 나쁜 음식은 X를 눌러주세요.", Toast.LENGTH_LONG).show();
+        ImageButton backBtn = (ImageButton) findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.load_fade_in, R.anim.load_fade_out);
+                finish();
+            }
+        });
+    }
+}
