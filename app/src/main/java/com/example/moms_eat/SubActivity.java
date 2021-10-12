1ec198a47eacc0769f4ab99e8fc5c8317dff08ee
Marline2 <Marline2@users.noreply.github.com>
2021-10-12 23:38:44 +0900

    *menu page* change menu's position
    *first menu page*
    add FirstPageActivity.java, first_page.xml
    add firsttoolbar.xml for toolbar

--- app/src/main/java/com/example/moms_eat/SubActivity.java
+++ app/src/main/java/com/example/moms_eat/SubActivity.java
@@ -6,6 +6,7 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -17,13 +18,26 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subactivity_main);
+
         Toast.makeText(this, "몸에 좋은 음식은 ♥를, \n 나쁜 음식은 X를 눌러주세요.", Toast.LENGTH_LONG).show();
+
         ImageButton backBtn = (ImageButton) findViewById(R.id.back_btn);
         backBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.load_fade_in, R.anim.load_fade_out);
+                finish();
+            }
+        });
+
+        Button firstPage = (Button) findViewById(R.id.fav1);
+        firstPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SubActivity.this, FirstPageActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.load_fade_in, R.anim.load_fade_out);
                 finish();
